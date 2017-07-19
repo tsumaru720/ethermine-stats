976449293053b3f83f8831ceda9ec7e60827ec67
Simon Mott <simon@simonmott.co.uk>
2017-07-19 14:11:12 +0100

    custom css isnt loaded correctly if you're already in a folder

--- inc/functions_core.php
+++ inc/functions_core.php
@@ -10,7 +10,7 @@ function core_output_head() {
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-	    <link href="/css/custom.css" rel="stylesheet">
+	    <link href="css/custom.css" rel="stylesheet">
 	    <!--[if lt IE 9]>
 	    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
