a37b43670c07ad4e6d6a6ac71a22aabdcac50165
Simon Mott <simon@simonmott.co.uk>
2017-08-08 21:24:32 +0100

    use favicon from folder instead of domain root

--- inc/functions_core.php
+++ inc/functions_core.php
@@ -12,6 +12,7 @@ function core_output_head() {
 	    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 	    <link href="css/custom.css" rel="stylesheet">
+	    <link href="favicon.ico" rel="shortcut icon">
 	    <!--[if lt IE 9]>
 	    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
